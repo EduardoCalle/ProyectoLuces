672d26ca6c0d47585a74f44d53da4f9d8e3ce293
Alicia Recalde <marijo.recalde@gmail.com>
2015-04-03 11:07:19 -0700

    Agregué el código del experimento con el sensor piezoeléctrico

---
+++ experiments/test_piezoElectrico/test_piezoElectrico.ino
@@ -0,0 +1,37 @@
+#include <Adafruit_DotStar.h>
+#include <SPI.h>  
+
+#define NUMPIXELS 145 
+#define PISO1 A0
+#define DATAPIN    4
+#define CLOCKPIN   6
+
+Adafruit_DotStar strip = Adafruit_DotStar(NUMPIXELS, DATAPIN, CLOCKPIN);
+
+
+void setup() {
+
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000L)
+  clock_prescale_set(clock_div_1); 
+#endif
+
+  strip.begin(); 
+  strip.show();  
+  Serial.begin(9600);
+}
+
+uint32_t color = 0xFF0000;      
+int segundo = 500;
+int cont = 1;
+
+void loop() {
+  int valorSensor = analogRead(PISO1);
+  Serial.println(valorSensor);
+  delay(1*segundo);
+  cont+=1;
+  if (cont>40) {
+    Serial.println("The End!");
+    exit(0);
+  }
+}
+ 
